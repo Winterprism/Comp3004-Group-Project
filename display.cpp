b31d422b8bf0c018c363137cffe5504df90c1c52
moissmart1 <69487872+moissmart1@users.noreply.github.com>
2023-11-23 13:19:49 -0500

    Update display.cpp

--- display.cpp
+++ display.cpp
@@ -1,6 +1,26 @@
 #include "display.h"
 
-Display::Display()
-{
+Display::Display(QTextEdit *display, QObject *parent):QObject(parent), d(display){}
 
+void Display::powerOn(){
+    d->setText("Powering On\n");
+    QTimer::singleShot(2000,this,&Display::electrodesCheck);
+    QTimer::singleShot(4000,this,&Display::batteriesCheck);
+}
+void Display::electrodesCheck(){
+    d->setText("Electrodes Are Correctelly Placed\n");
+}
+void Display::batteriesCheck(){
+    d->setText("Batteries are installed");
+}
+
+
+void Display::powerOff(){
+    d->setText("Powering Off\n");
+}
+
+
+
+void Display::replaceB(){
+    d->setText("Batteries need to be replaced");
 }
