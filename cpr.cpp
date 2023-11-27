66326e37f3893264e87063501ca5ccf1eb82a33d
Antony Ren <44201363+tonyren-card@users.noreply.github.com>
2023-11-27 10:52:56 -0500

    More variables and functions added to cpr.cpp
    included isPerforming variable as well as perform() and stop() functions

--- cpr.cpp
+++ cpr.cpp
@@ -3,13 +3,22 @@
 CPR::CPR()
 {
     this->enabled = false;
+    this->isPerforming = false;
 }
 
-void CPR::enableCPR()
+void CPR::enable()
 {
     this->enabled = true;
 }
 
-bool CPR::isCPREnabled(){
+bool CPR::isEnabled(){
     return this->enabled;
+}
+
+void CPR::perform(){
+    this->isPerforming = true;
+}
+
+void CPR::stop(){
+    this->isPerforming = false;
 }
