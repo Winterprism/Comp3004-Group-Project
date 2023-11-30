9b7f5eddbf896417018a68092e0b82f8d8ba3f53
Kevin Zhang <kevinzhang3303@gmail.com>
2023-11-30 14:39:19 -0500

    Added functionality to new functions in display.cpp

--- display.cpp
+++ display.cpp
@@ -6,6 +6,8 @@ void Display::powerOn(){
     d->setText("Powering On\n");
     QTimer::singleShot(2000,this,&Display::electrodesCheck);
     QTimer::singleShot(4000,this,&Display::batteriesCheck);
+    QTimer::singleShot(5000,this,&Display::responsivenesCheck);
+    QTimer::singleShot(8000,this,&Display::call911);
 }
 void Display::electrodesCheck(){
     d->setText("Electrodes Are Correctly Placed\n");
@@ -20,7 +22,17 @@ void Display::powerOff(){
 }
 
 
-
 void Display::replaceB(){
     d->setText("Batteries need to be replaced");
+}
+
+
+void Display::responsivenesCheck(){
+    d->clear();
+    d->setText("Checking for responsiveness...");
+}
+
+void Display::call911(){
+    d->clear();
+    d->setText("Calling 911...");
 }
