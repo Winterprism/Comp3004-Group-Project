e159df259933d7f151cd2c328ac16196e33fe971
Antony Ren <44201363+tonyren-card@users.noreply.github.com>
2023-11-30 19:58:12 -0500

    New analyze function and deleted some old code cpr.cpp

--- cpr.cpp
+++ cpr.cpp
@@ -1,24 +1,43 @@
 #include "cpr.h"
 
-CPR::CPR()
+CPR::CPR(QTextEdit *display, QObject *parent):QObject(parent), d(display)
 {
-    this->enabled = false;
-    this->isPerforming = false;
+    this->timer = new QElapsedTimer();
 }
 
-void CPR::enable()
-{
-    this->enabled = true;
+/* Button press interval:
+ * - intervals always last 3 seconds
+ * - >=5 presses in 3 seconds is too fast, display will indicate to slow down (red light will flash)
+ * - 1 press in 3 seconds is too slow, display indicates
+*/
+
+void CPR::trackPresses(){
+    if (counter == 0) {
+        timer->start();
+        d->setText("Performing CPR");
+    }
+
+    if (!timer->hasExpired(10000)) {
+        counter++;
+    }else{
+        this->analyzeInterval();
+    }
 }
 
-bool CPR::isEnabled(){
-    return this->enabled;
-}
+void CPR::analyzeInterval(){
+    //Assuming the interval is 10 seconds
 
-void CPR::perform(){
-    this->isPerforming = true;
-}
-
-void CPR::stop(){
-    this->isPerforming = false;
+    // when pressing speed is too fast (>=5)
+    if (counter > 15) {
+        d->setText("Slow down!");
+    } //when pressing speed is too slow (<=1)
+    else if (counter < 5){
+        d->setText("Press faster");
+        // set led_indicator to red
+    } //when pressing speed is just right
+    else{
+        d->setText("CPR performing well");
+        // set led_indicator to green
+    }
+    counter = 0;
 }
