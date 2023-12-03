f6ee1d2264304d3abd851304f42f111e94ccdc52
Antony Ren <44201363+tonyren-card@users.noreply.github.com>
2023-12-03 10:23:46 -0500

    colour indication coded to cpr.cpp + updates
    changed print statements in analyzeinterval() to append, trackpresses() to call analyzeinterval() after 9 seconds of presses

--- cpr.cpp
+++ cpr.cpp
@@ -14,10 +14,10 @@ CPR::CPR(QTextEdit *display, QObject *parent):QObject(parent), d(display)
 void CPR::trackPresses(){
     if (counter == 0) {
         timer->start();
-        d->setText("Performing CPR");
+        d->append("Performing CPR\n");
     }
 
-    if (!timer->hasExpired(10000)) {
+    if (!timer->hasExpired(9000)) {
         counter++;
     }else{
         this->analyzeInterval();
@@ -29,15 +29,23 @@ void CPR::analyzeInterval(){
 
     // when pressing speed is too fast (>=5)
     if (counter > 15) {
-        d->setText("Slow down!");
+        d->append("Slow down!\n");
+        colorIndc = 1;
+        getIndcColor(); //red
     } //when pressing speed is too slow (<=1)
     else if (counter < 5){
-        d->setText("Press faster");
-        // set led_indicator to red
+        d->append("Press faster\n");
+        colorIndc = 1;
+        getIndcColor(); //red
     } //when pressing speed is just right
     else{
-        d->setText("CPR performing well");
-        // set led_indicator to green
+        d->append("CPR performing well\n");
+        colorIndc = 2;
+        getIndcColor(); //green
     }
     counter = 0;
+}
+
+int CPR::getIndcColor(){
+    return colorIndc;
 }
